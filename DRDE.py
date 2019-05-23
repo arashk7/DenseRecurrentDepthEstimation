a8e87242bd609cbce5550fc564b7a1e6b473bf4f
arashk7 <ahmad.karambakhsh@gmail.com>
2019-05-23 10:16:26 +0800

    Update DRDE.py

--- DRDE.py
+++ DRDE.py
@@ -27,74 +27,11 @@ class DRDE:
     def init_model(self):
         Input_1 = Input(shape=(100, 150, 1), name='Input_1')
         Input_2 = Input(shape=(100, 150, 1), name='Input_2')
-        merge_1 = Concatenate()([Input_1, Input_2])
-        Reshape_1 = Reshape(name='Reshape_1', target_shape=(2, 100, 150, 1))(merge_1)
-        ConvLSTM2D_1 = ConvLSTM2D(name='ConvLSTM2D_1', nb_col=5, border_mode='same', nb_filter=32, activation='relu',
-                                  nb_row=5, subsample=(2, 2), dim_ordering='tf', return_sequences=True)(Reshape_1)
-        Dropout_21 = Dropout(name='Dropout_21', p=0.2)(ConvLSTM2D_1)
-        ConvLSTM2D_2 = ConvLSTM2D(name='ConvLSTM2D_2', nb_col=5, subsample=(2, 2), nb_filter=32, activation='relu',
-                                  nb_row=5, dim_ordering='tf', return_sequences=True)(Dropout_21)
-        Dropout_22 = Dropout(name='Dropout_22', p=0.2)(ConvLSTM2D_2)
-        ConvLSTM2D_3 = ConvLSTM2D(name='ConvLSTM2D_3', nb_col=5, border_mode='same', nb_filter=32, activation='relu',
-                                  nb_row=5, subsample=(2, 2), dim_ordering='tf')(Dropout_22)
-        Convolution2D_21 = Convolution2D(name='Convolution2D_21', nb_col=5, nb_filter=1, border_mode='same', nb_row=5)(
-            Input_2)
-        Convolution2D_11 = Convolution2D(name='Convolution2D_11', nb_col=5, nb_filter=16, border_mode='same', nb_row=5)(
-            Input_2)
-        MaxPooling2D_3 = MaxPooling2D(name='MaxPooling2D_3', pool_size=(8, 8))(Convolution2D_11)
-        Dropout_23 = Dropout(name='Dropout_23', p=0.2)(ConvLSTM2D_3)
-        merge_5 = Concatenate()([MaxPooling2D_3, Dropout_23])
-        BatchNormalization_3 = BatchNormalization(name='BatchNormalization_3')(merge_5)
-        Activation_3 = Activation(name='Activation_3', activation='relu')(BatchNormalization_3)
-        Convolution2D_12 = Convolution2D(name='Convolution2D_12', nb_col=1, nb_filter=64, border_mode='same', nb_row=1)(
-            Activation_3)
-        BatchNormalization_4 = BatchNormalization(name='BatchNormalization_4')(Convolution2D_12)
-        Activation_4 = Activation(name='Activation_4', activation='relu')(BatchNormalization_4)
-        Convolution2D_13 = Convolution2D(name='Convolution2D_13', nb_col=5, nb_filter=16, border_mode='same', nb_row=5)(
-            Activation_4)
-        Dropout_9 = Dropout(name='Dropout_9', p=0.2)(Convolution2D_13)
-        merge_6 = Concatenate()([Dropout_9, merge_5])
-        BatchNormalization_5 = BatchNormalization(name='BatchNormalization_5')(merge_6)
-        Activation_5 = Activation(name='Activation_5', activation='relu')(BatchNormalization_5)
-        Convolution2D_14 = Convolution2D(name='Convolution2D_14', nb_col=1, nb_filter=64, border_mode='same', nb_row=1)(
-            Activation_5)
-        BatchNormalization_6 = BatchNormalization(name='BatchNormalization_6')(Convolution2D_14)
-        Activation_6 = Activation(name='Activation_6', activation='relu')(BatchNormalization_6)
-        Convolution2D_15 = Convolution2D(name='Convolution2D_15', nb_col=5, nb_filter=16, border_mode='same', nb_row=5)(
-            Activation_6)
-        Dropout_10 = Dropout(name='Dropout_10', p=0.2)(Convolution2D_15)
-        merge_7 = Concatenate()([Dropout_10, merge_6])
-        BatchNormalization_7 = BatchNormalization(name='BatchNormalization_7')(merge_7)
-        Activation_7 = Activation(name='Activation_7', activation='relu')(BatchNormalization_7)
-        Convolution2D_16 = Convolution2D(name='Convolution2D_16', nb_col=1, nb_filter=64, border_mode='same', nb_row=1)(
-            Activation_7)
-        BatchNormalization_8 = BatchNormalization(name='BatchNormalization_8')(Convolution2D_16)
-        Activation_8 = Activation(name='Activation_8', activation='relu')(BatchNormalization_8)
-        Convolution2D_17 = Convolution2D(name='Convolution2D_17', nb_col=5, nb_filter=16, border_mode='same', nb_row=5)(
-            Activation_8)
-        Dropout_11 = Dropout(name='Dropout_11', p=0.2)(Convolution2D_17)
-        merge_8 = Concatenate()([Dropout_11, merge_7])
-        BatchNormalization_9 = BatchNormalization(name='BatchNormalization_9')(merge_8)
-        Activation_9 = Activation(name='Activation_9', activation='relu')(BatchNormalization_9)
-        Convolution2D_18 = Convolution2D(name='Convolution2D_18', nb_col=1, nb_filter=64, border_mode='same', nb_row=1)(
-            Activation_9)
-        BatchNormalization_10 = BatchNormalization(name='BatchNormalization_10')(Convolution2D_18)
-        Activation_10 = Activation(name='Activation_10', activation='relu')(BatchNormalization_10)
-        Convolution2D_19 = Convolution2D(name='Convolution2D_19', nb_col=5, nb_filter=16, border_mode='same', nb_row=5)(
-            Activation_10)
-        Dropout_12 = Dropout(name='Dropout_12', p=0.2)(Convolution2D_19)
-        merge_9 = Concatenate()([Dropout_12, merge_8])
-        BatchNormalization_11 = BatchNormalization(name='BatchNormalization_11')(merge_9)
-        Activation_11 = Activation(name='Activation_11', activation='relu')(BatchNormalization_11)
-        Convolution2D_20 = Convolution2D(name='Convolution2D_20', nb_col=1, nb_filter=32, border_mode='same', nb_row=1)(
-            Activation_11)
-        UpSampling2D_1 = UpSampling2D(name='UpSampling2D_1', size=(8, 8))(Convolution2D_20)
-        ZeroPadding2D_1 = ZeroPadding2D(name='ZeroPadding2D_1', padding=(2, 3))(UpSampling2D_1)
-        merge_10 = Concatenate(axis=3)([ZeroPadding2D_1, Convolution2D_21])
-        BatchNormalization_12 = BatchNormalization(name='BatchNormalization_12')(merge_10)
-        Activation_12 = Activation(name='Activation_12', activation='relu')(BatchNormalization_12)
-        Convolution2D_22 = Convolution2D(name='Convolution2D_22', nb_col=5, nb_filter=16, border_mode='same', nb_row=5)(
-            Activation_12)
+        # 
+        #
+        # This section will be shown after publishing the paper
+        #
+        #
         merge_11 = Concatenate(axis=3)([merge_10, Convolution2D_22])
         BatchNormalization_13 = BatchNormalization(name='BatchNormalization_13')(merge_11)
         Activation_13 = Activation(name='Activation_13', activation='relu')(BatchNormalization_13)
